a9414818945f671a2c50a0c892b84646803e5875
pih6585 <pih6585@naver.com>
2021-10-01 15:43:00 +0900

    refactor : status enum으로 변경

--- src/main/java/baseball/model/Status.java
+++ src/main/java/baseball/model/Status.java
@@ -1,68 +1,20 @@
 package baseball.model;
 
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-public class Status {
-	private static final int PLUS_NUMBER = 1;
-	private static final int ZERO_POINT = 0;
-	private static final int PERFECT_CONDITION = 3;
+public enum Status {
 
-	private final int strike;
-	private final int ball;
+	STRIKE, BALL, NOTHING;
 
-	private Status(int strike, int ball) {
-		this.strike = strike;
-		this.ball = ball;
+	public static List<Status> getValues() {
+		List<Status> values = new ArrayList<>();
+		Collections.addAll(values, Status.values());
+		return values;
 	}
 
-	public static Status init() {
-		return new Status(ZERO_POINT, ZERO_POINT);
-	}
-
-	public Status strike() {
-		return new Status(strike + PLUS_NUMBER, ball);
-	}
-
-	public Status ball() {
-		return new Status(strike, ball + PLUS_NUMBER);
-	}
-
-	public boolean isNothing() {
-		return strike == ZERO_POINT && ball == ZERO_POINT;
-	}
-
-	public boolean isPerfectStrike() {
-		return strike == PERFECT_CONDITION;
-	}
-
-	public boolean existStrike() {
-		return strike > ZERO_POINT;
-	}
-
-	public boolean existBall() {
-		return ball > ZERO_POINT;
-	}
-
-	public int getStrike() {
-		return strike;
-	}
-
-	public int getBall() {
-		return ball;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
-		Status status = (Status)o;
-		return strike == status.strike && ball == status.ball;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(strike, ball);
+	public static boolean isNotNothing(Status status) {
+		return status != NOTHING;
 	}
 }
--- src/test/java/baseball/model/StatusTest.java
+++ src/test/java/baseball/model/StatusTest.java
@@ -8,59 +8,8 @@ import org.junit.jupiter.api.Test;
 class StatusTest {
 
 	@Test
-	@DisplayName("게임의 상황을 초기화 한다.")
-	public void initStatus() {
-		Status status = Status.init();
-
-		assertThat(status).isEqualTo(Status.init());
-	}
-
-	@Test
-	@DisplayName("스트라이크이면 스트라이크 점수가 1점씩 올라간다.")
-	public void strike() {
-		Status status = Status.init();
-		status = status.strike();
-		assertThat(status.getStrike()).isEqualTo(1);
-	}
-
-	@Test
-	@DisplayName("볼이면 볼의 점수가 1점씩 올라간다.")
-	public void ball() {
-		Status status = Status.init();
-		status = status.ball();
-		assertThat(status.getBall()).isEqualTo(1);
-	}
-
-	@Test
-	@DisplayName("스트라이크와 볼이 0 이면 낫싱이다.")
-	public void isNothing() {
-		Status status = Status.init();
-		assertThat(status.isNothing()).isTrue();
-	}
-
-	@Test
-	@DisplayName("스트라이크가 3개이면 퍼펙트 이다.")
-	public void isPerfectStrike() {
-		Status status = Status.init();
-		status = status.strike();
-		status = status.strike();
-		status = status.strike();
-		assertThat(status.isPerfectStrike()).isTrue();
-	}
-
-	@Test
-	@DisplayName("스트라이크가 1개라도 있으면 스트라이크가 존재한다.")
-	public void existStrike() {
-		Status status = Status.init();
-		status = status.strike();
-		assertThat(status.existStrike()).isTrue();
-	}
-
-	@Test
-	@DisplayName("볼이 1개라도 있으면 볼이 존재한다.")
-	public void existBall() {
-		Status status = Status.init();
-		status = status.ball();
-		assertThat(status.existBall()).isTrue();
+	@DisplayName("status는 strike, ball, nothing을 포함하고있다.")
+	public void containsValues() {
+		assertThat(Status.getValues()).contains(Status.STRIKE, Status.BALL, Status.NOTHING);
 	}
 }
