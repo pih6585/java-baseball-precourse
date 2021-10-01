d3af17d969e9921e7afbb3bc87cb37f76609b268
pih6585 <pih6585@naver.com>
2021-10-01 10:42:01 +0900

    feat : controller 추가

---
+++ src/main/java/baseball/controller/Controller.java
@@ -0,0 +1,43 @@
+package baseball.controller;
+
+import baseball.generator.BallsGenerator;
+import baseball.model.Ball;
+import baseball.model.Balls;
+import baseball.model.BaseBallGame;
+import baseball.model.GameReStart;
+import baseball.model.Status;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+public class Controller {
+
+	public void run() {
+		boolean isGameEnd = gameStart(BallsGenerator.createTargetBalls());
+
+		while (isReGame() && isGameEnd) {
+			isGameEnd = gameStart(BallsGenerator.createTargetBalls());
+		}
+
+		OutputView.printEndGame();
+	}
+
+	private boolean gameStart(Balls targetBalls) {
+		BaseBallGame baseBallGame = new BaseBallGame(targetBalls);
+
+		Status status = baseBallGame.play(BallsGenerator.createCustomBalls(InputView.inputBalls()));
+
+		while (baseBallGame.isContinue()) {
+			OutputView.printGameResult(status);
+			status = baseBallGame.play(BallsGenerator.createCustomBalls(InputView.inputBalls()));
+		}
+
+		OutputView.printGameResult(status);
+
+		return !baseBallGame.isContinue();
+	}
+
+	private boolean isReGame() {
+		return new GameReStart(InputView.inputChoiceReGame()).isReGame();
+	}
+
+}
