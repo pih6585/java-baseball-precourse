3188a1e572e362d864367ba20fa9bf826e82523c
pih6585 <pih6585@naver.com>
2021-10-01 10:40:33 +0900

    feat : 볼에 대한 일급 컬렉션 추가

---
+++ src/main/java/baseball/model/Balls.java
@@ -0,0 +1,71 @@
+package baseball.model;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+public class Balls {
+	private static final String CHECK_BALLS_SIZE_ERROR_MESSAGE = "야구게임의 볼은 3자리 입니다.";
+	private static final String CHECK_DUPLICATION_ERROR_MESSAGE = "중복된 숫자가 존재 합니다.";
+	private static final int BALLS_SIZE = 3;
+
+	private final List<Ball> balls;
+
+	public Balls(List<Ball> balls) {
+		checkBallsSize(balls);
+		checkDuplication(balls);
+		this.balls = Collections.unmodifiableList(balls);
+	}
+
+	public Status play(Ball customBalls, int position, Status status) {
+		if (balls.contains(customBalls) && findBall(position).equals(customBalls)) {
+			return status.strike();
+		}
+		if (balls.contains(customBalls)) {
+			return status.ball();
+		}
+		return status;
+	}
+
+	private Ball findBall(int position) {
+		return balls.get(position);
+	}
+
+	private void checkDuplication(List<Ball> balls) {
+		if (balls.size() != removeDuplication(balls)) {
+			throw new IllegalArgumentException(CHECK_DUPLICATION_ERROR_MESSAGE);
+		}
+	}
+
+	private int removeDuplication(List<Ball> balls) {
+		Set<Ball> uniqueValues = new HashSet<>(balls);
+		return uniqueValues.size();
+	}
+
+	private void checkBallsSize(List<Ball> balls) {
+		if (balls.size() != BALLS_SIZE) {
+			throw new IllegalArgumentException(CHECK_BALLS_SIZE_ERROR_MESSAGE);
+		}
+	}
+
+	public List<Ball> getBalls() {
+		return balls;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Balls balls1 = (Balls)o;
+		return Objects.equals(balls, balls1.balls);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(balls);
+	}
+}
---
+++ src/test/java/baseball/model/BallsTest.java
@@ -0,0 +1,72 @@
+package baseball.model;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import baseball.generator.BallsGenerator;
+
+class BallsTest {
+
+	@Test
+	@DisplayName("3자리의 볼을 생성한다.")
+	public void createBalls() {
+		Balls balls = new Balls(Arrays.asList(new Ball(1), new Ball(2), new Ball(3)));
+
+		assertThat(balls).isEqualTo(new Balls(Arrays.asList(new Ball(1), new Ball(2), new Ball(3))));
+	}
+
+	@Test
+	@DisplayName("볼은 3자리가 아니면 예외가 발생한다.")
+	public void checkBallsSize() {
+		assertThrows(IllegalArgumentException.class,
+			() -> new Balls(Arrays.asList(new Ball(1), new Ball(2))));
+	}
+
+	@Test
+	@DisplayName("볼의 숫자가 중복되면 예외가 발생한다.")
+	public void checkDuplication() {
+		assertThrows(IllegalArgumentException.class,
+			() -> new Balls(Arrays.asList(new Ball(1), new Ball(1), new Ball(1))));
+	}
+
+	@Test
+	@DisplayName("볼과 볼의 자리수와 값이 일치하면 스트라이크이다.")
+	public void playStrike() {
+		Balls targetBalls = BallsGenerator.createCustomBalls("123");
+		Balls customBalls = BallsGenerator.createCustomBalls("145");
+		Status status = Status.init();
+		Status result = targetBalls.play(customBalls.getBalls().get(0), 0, status);
+
+		assertThat(result.getStrike()).
+
+			isEqualTo(1);
+
+	}
+
+	@Test
+	@DisplayName("볼과 볼의 값이 일치하고 위치가 다르면 볼이다.")
+	public void playBall() {
+		Balls targetBalls = BallsGenerator.createCustomBalls("123");
+		Balls customBalls = BallsGenerator.createCustomBalls("415");
+		Status status = Status.init();
+		Status result = targetBalls.play(customBalls.getBalls().get(1), 1, status);
+
+		assertThat(result.getBall()).isEqualTo(1);
+	}
+
+	@Test
+	@DisplayName("볼과 볼의 값와 위치가 다르면 nothing 이다.")
+	public void playNothing() {
+		Balls targetBalls = BallsGenerator.createCustomBalls("123");
+		Balls customBalls = BallsGenerator.createCustomBalls("465");
+		Status status = Status.init();
+		Status result = targetBalls.play(customBalls.getBalls().get(0), 0, status);
+
+		assertThat(result).isEqualTo(Status.init());
+	}
+}
