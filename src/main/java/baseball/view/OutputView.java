a214555c43f53ce9774df733b2226522c958eb44
pih6585 <pih6585@naver.com>
2021-10-02 19:03:17 +0900

    refactor : output result 출력 수정

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -7,10 +7,8 @@ public class OutputView {
 	private static final String STRIKE_MESSAGE = "스트라이크";
 	private static final String BALL_MESSAGE = "볼";
 	private static final String NOTHING_MESSAGE = "낫싱";
-	private static final String EMPTY_MESSAGE = "";
 	private static final String END_GAME_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 끝";
 	private static final String RE_END_GAME_MESSAGE = "게임이 종료 되었습니다.";
-
 	private static final int ZERO_POINT = 0;
 
 	public static void printGameResult(GameResult gameResult) {
@@ -26,15 +24,13 @@ public class OutputView {
 	}
 
 	public static void printGameReview(GameResult gameResult) {
-		String strikeMessage = EMPTY_MESSAGE;
-		String ballMessage = EMPTY_MESSAGE;
 		if (gameResult.countStrike() > ZERO_POINT) {
-			strikeMessage = gameResult.countStrike() + STRIKE_MESSAGE;
+			System.out.print(gameResult.countStrike() + STRIKE_MESSAGE + " ");
 		}
 		if (gameResult.countBall() > ZERO_POINT) {
-			ballMessage = gameResult.countBall() + BALL_MESSAGE;
+			System.out.print(gameResult.countBall() + BALL_MESSAGE);
 		}
-		System.out.println(strikeMessage + " " + ballMessage);
+		System.out.print(System.lineSeparator());
 	}
 
 	public static void printEndGame() {
